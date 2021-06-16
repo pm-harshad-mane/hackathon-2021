34494abc369d66c6b9df8cbc35e98e8f72d3f0bb
Harshad Mane <harshad.mane@pubmatic.com>
2021-06-16 13:17:35 -0700

    new current version with responseProvider

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
 // import { logger } from 'log'
 import URLSearchParams from 'url-search-params';
-// import { createResponse } from 'create-response';
+import { createResponse } from 'create-response';
 import { getRecordFromFakeKV } from 'data.js';
 import { getDeviceType, evaluateIt } from 'evaluate.js'
 
-const version = '0.45';
+const version = '0.46';
 
-export function onClientRequest (request) {
+// export function onClientRequest (request) {
+//     const params = new URLSearchParams(request.query)
+//     const publisherId = params.get('publisherId');
+//     const profileGroupId = params.get('profileGroupId');
+//     const kvKeyName = publisherId + '___' + profileGroupId;
+//     const deviceType = getDeviceType(request);
+//     const dataFromKV = getRecordFromFakeKV(kvKeyName);
+//     const op = evaluateIt(request, dataFromKV || {});
+
+//     request.respondWith(
+//         200, 
+//         {},
+//         `<html>
+//             <body>
+//                 <h1>Harshad: Hackathon 2021!</h1>
+//                 version: ${version}<br>
+//                 publisherId: ${publisherId}<br>
+//                 profileGroupId: ${profileGroupId}<br>
+//                 kvKeyName: ${kvKeyName}<br>
+//                 deviceType: ${deviceType}<br>
+//                 dataFromKV: ${JSON.stringify(dataFromKV)}<br>
+//                 op: ${JSON.stringify(op)}
+//             </body>
+//         </html>`
+//     );
+// }
+
+export async function responseProvider(request) {
     const params = new URLSearchParams(request.query)
     const publisherId = params.get('publisherId');
     const profileGroupId = params.get('profileGroupId');
     const kvKeyName = publisherId + '___' + profileGroupId;
     const deviceType = getDeviceType(request);
-    // const deviceType = "UNKNOWN DEVICE";
     const dataFromKV = getRecordFromFakeKV(kvKeyName);
-    // const dataFromKV = {};
     const op = evaluateIt(request, dataFromKV || {});
-    // const op = buzz();
-
-    request.respondWith(
-        200, 
-        {},
+    
+    return createResponse(
+        200,
+        {'Content-Type':['text/html;charset=utf-8']},
         `<html>
             <body>
                 <h1>Harshad: Hackathon 2021!</h1>
@@ -35,31 +59,3 @@ export function onClientRequest (request) {
         </html>`
     );
 }
-
-// export async function responseProvider(request) {
-//     const params = new URLSearchParams(request.query)
-//     const publisherId = params.get('publisherId');
-//     const profileGroupId = params.get('profileGroupId');
-//     const kvKeyName = publisherId + '___' + profileGroupId;
-//     // const deviceType = getDeviceType(request);
-//     // const dataFromKV = getRecordFromFakeKV(kvKeyName);
-//     // const op = evaluate(request, dataFromKV);
-    
-//     var data = "NOTHING!!!"; 
-
-//     return createResponse(
-//         200,
-//         {'Content-Type':['text/html;charset=utf-8']},
-//         // `hello world... Chashma!!! ${version}!<br>${data}`
-//         `<html>
-//             <body>
-//                 <h1>Harshad: Hackathon 2021... LOLZZ!</h1>
-//                 version: ${version}<br>
-//                 data: ${data}<br>
-//                 publisherId: ${publisherId}<br>
-//                 profileGroupId: ${profileGroupId}<br>
-//                 kvKeyName: ${kvKeyName}<br>                    
-//             </body>
-//         </html>`
-//     );
-// }
