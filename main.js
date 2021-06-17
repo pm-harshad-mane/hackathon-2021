56c714446b01eaa357938114c44c4b1c28b8caad
Harshad Mane <harshad.mane@pubmatic.com>
2021-06-16 22:11:13 -0700

    added debug mode as well; current live version

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ import { EdgeKV } from './edgekv.js';
 import { getRecordFromFakeKV } from 'data.js';
 import { getDeviceType, evaluateIt } from 'evaluate.js'
 
-const version = '0.54';
+const version = '0.61';
 
 export async function onClientRequest (request) {
     const params = new URLSearchParams(request.query)
     const publisherId = params.get('publisherId');
     const profileGroupId = params.get('profileGroupId');
+    const doNotRedirect = params.get('doNotRedirect');
     const kvKeyName = publisherId + '___' + profileGroupId;
     const deviceType = getDeviceType(request);
     // const dataFromKV = getRecordFromFakeKV(kvKeyName);
@@ -30,26 +31,40 @@ export async function onClientRequest (request) {
         dataFromKV = {}; // error.toString();
     }
 
-    const op = evaluateIt(request, dataFromKV || {});
-    request.respondWith(
-        200,
-        {
-            'Content-Type':['text/html;charset=utf-8'],
-            'Cache-Control': 'max-age=0, must-revalidate'
-        },
-        `<html>
-            <body>
-                <h1>Harshad: Hackathon 2021!!</h1>
-                version: ${version}<br>
-                publisherId: ${publisherId}<br>
-                profileGroupId: ${profileGroupId}<br>
-                kvKeyName: ${kvKeyName}<br>
-                deviceType: ${deviceType}<br>
-                dataFromKV: ${JSON.stringify(dataFromKV)}<br>
+    var op = evaluateIt(request, dataFromKV || {});
+
+    if(op && doNotRedirect != 1){
+        request.respondWith(
+            302,
+            {
+                Location: [`https://ads.pubmatic.com/AdServer/js/pwt/${publisherId}/${op}/pwt.js`]
+            },
+            ''
+        );
+    } else {
+        request.respondWith(
+            200,
+            {
+                'Content-Type':['text/javascript;charset=utf-8'],
+                'Cache-Control': 'max-age=0, must-revalidate'
+            },
+            `
+                /*
+                Harshad: Hackathon 2021!!
+                version: ${version}
+                publisherId: ${publisherId}
+                profileGroupId: ${profileGroupId}
+                kvKeyName: ${kvKeyName}
+                deviceType: ${deviceType}
+                dataFromKV: ${JSON.stringify(dataFromKV)}
+                request.userLocation: ${JSON.stringify(request.userLocation)}
                 op: ${op}
-            </body>
-        </html>`
-    );
+                */
+            `
+        );
+    }
+
+    
 }
 
 // export async function responseProvider(request) {
