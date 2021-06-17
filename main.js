1c78151a3b467bb4df971212d9c161f2ba8c4f01
Harshad Mane <harshad.mane@pubmatic.com>
2021-06-16 19:34:10 -0700

    now working with KV

--- main.js
+++ main.js
@@ -1,53 +1,45 @@
 // import { logger } from 'log'
 import URLSearchParams from 'url-search-params';
-import { createResponse } from 'create-response';
+// import { createResponse } from 'create-response';
+import { EdgeKV } from './edgekv.js';
 import { getRecordFromFakeKV } from 'data.js';
 import { getDeviceType, evaluateIt } from 'evaluate.js'
 
-const version = '0.46';
+const version = '0.53';
 
-// export function onClientRequest (request) {
-//     const params = new URLSearchParams(request.query)
-//     const publisherId = params.get('publisherId');
-//     const profileGroupId = params.get('profileGroupId');
-//     const kvKeyName = publisherId + '___' + profileGroupId;
-//     const deviceType = getDeviceType(request);
-//     const dataFromKV = getRecordFromFakeKV(kvKeyName);
-//     const op = evaluateIt(request, dataFromKV || {});
-
-//     request.respondWith(
-//         200, 
-//         {},
-//         `<html>
-//             <body>
-//                 <h1>Harshad: Hackathon 2021!</h1>
-//                 version: ${version}<br>
-//                 publisherId: ${publisherId}<br>
-//                 profileGroupId: ${profileGroupId}<br>
-//                 kvKeyName: ${kvKeyName}<br>
-//                 deviceType: ${deviceType}<br>
-//                 dataFromKV: ${JSON.stringify(dataFromKV)}<br>
-//                 op: ${JSON.stringify(op)}
-//             </body>
-//         </html>`
-//     );
-// }
-
-export async function responseProvider(request) {
+export async function onClientRequest (request) {
     const params = new URLSearchParams(request.query)
     const publisherId = params.get('publisherId');
     const profileGroupId = params.get('profileGroupId');
     const kvKeyName = publisherId + '___' + profileGroupId;
     const deviceType = getDeviceType(request);
-    const dataFromKV = getRecordFromFakeKV(kvKeyName);
+    // const dataFromKV = getRecordFromFakeKV(kvKeyName);
+
+    const edgeKv = new EdgeKV({namespace: "ow-profile-selector", group: "pub-profile-groups"});
+    // var data = "NO_DATA_FOUND";
+    var dataFromKV = {};
+    try {
+        dataFromKV = await edgeKv.getText({
+            item: kvKeyName, 
+            default_value: {} 
+        });
+        dataFromKV = JSON.parse(dataFromKV.replace(/\\/g, ''));
+    } catch (error) {
+        // Catch the error and store the error message to use in a response
+        // header for debugging. Use a default greeting as well in this case.
+        dataFromKV = {}; // error.toString();
+    }
+
     const op = evaluateIt(request, dataFromKV || {});
-    
-    return createResponse(
+    request.respondWith(
         200,
-        {'Content-Type':['text/html;charset=utf-8']},
+        {
+            'Content-Type':['text/html;charset=utf-8'],
+            'Cache-Control': 'max-age=0, must-revalidate'
+        },
         `<html>
             <body>
-                <h1>Harshad: Hackathon 2021!</h1>
+                <h1>Harshad: Hackathon 2021!!</h1>
                 version: ${version}<br>
                 publisherId: ${publisherId}<br>
                 profileGroupId: ${profileGroupId}<br>
@@ -59,3 +51,49 @@ export async function responseProvider(request) {
         </html>`
     );
 }
+
+// export async function responseProvider(request) {
+//     const params = new URLSearchParams(request.query)
+//     const publisherId = params.get('publisherId');
+//     const profileGroupId = params.get('profileGroupId');
+//     const kvKeyName = publisherId + '___' + profileGroupId;
+//     const deviceType = getDeviceType(request);
+//     // const dataFromKV = getRecordFromFakeKV(kvKeyName);
+
+//     const edgeKv = new EdgeKV({namespace: "ow-profile-selector", group: "pub-profile-groups"});
+//     // var data = "NO_DATA_FOUND";
+//     var dataFromKV = {};
+//     try {
+//         dataFromKV = await edgeKv.getText({
+//             item: kvKeyName, 
+//             default_value: {} 
+//         });
+//         // dataFromKV = JSON.parse(dataFromKV);
+//     } catch (error) {
+//         // Catch the error and store the error message to use in a response
+//         // header for debugging. Use a default greeting as well in this case.
+//         dataFromKV = {}; // error.toString();
+//     }
+
+//     const op = evaluateIt(request, dataFromKV || {});
+    
+//     return createResponse(
+//         200,
+//         {
+//             'Content-Type':['text/html;charset=utf-8'],
+//             'Cache-Control': 'max-age=0, must-revalidate'
+//         },
+//         `<html>
+//             <body>
+//                 <h1>Harshad: Hackathon 2021!!</h1>
+//                 version: ${version}<br>
+//                 publisherId: ${publisherId}<br>
+//                 profileGroupId: ${profileGroupId}<br>
+//                 kvKeyName: ${kvKeyName}<br>
+//                 deviceType: ${deviceType}<br>
+//                 dataFromKV: ${JSON.stringify(dataFromKV)}<br>
+//                 op: ${JSON.stringify(op)}
+//             </body>
+//         </html>`
+//     );
+// }
